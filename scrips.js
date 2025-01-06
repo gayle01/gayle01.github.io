47bc572f9e8243780f1496b0af55321883702710
Gayle <gayleagbozoh6@gmail.com>
2025-01-06 21:11:40 +0000

    Add files via upload

---
+++ scrips.js
@@ -0,0 +1,15 @@
+<script>
+    var swiper = new Swiper(".hero-container", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7000;
+    {"}"}   disableOnInteraction: false,
+    };
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop:true,
+    });
+</script>
